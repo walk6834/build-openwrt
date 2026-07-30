58f7bcd00b566e5a8847409517dee13caea50b49
walk6834 <glacier0315@126.com>
2026-07-30 17:19:26 +0800

    style: 统一格式化所有shell脚本的缩进与空格
    调整了scripts目录下和根目录的shell脚本代码缩进，统一使用tab缩进，修正了重定向文件路径前后的空格，让代码风格更一致规范

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -7,9 +7,9 @@ COUNTRY_URL="https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/releas
 GEOIP_URL="https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
 GEOSITE_URL="https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
 
-wget -qO- $CLASH_META_URL | tar xOz > files/etc/openclash/core/clash_meta
-wget -qO- $COUNTRY_URL > files/etc/openclash/Country.mmdb
-wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+wget -qO- $CLASH_META_URL | tar xOz >files/etc/openclash/core/clash_meta
+wget -qO- $COUNTRY_URL >files/etc/openclash/Country.mmdb
+wget -qO- $GEOIP_URL >files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL >files/etc/openclash/GeoSite.dat
 
 chmod +x files/etc/openclash/core/clash*
